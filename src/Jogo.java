4356a6da1812e6b7efb757418470a4dbce252e03
beatrizpaivafelix <140505743+beatrizpaivafelix@users.noreply.github.com>
2025-10-30 22:27:31 -0300

    começando classe jogo

--- src/Jogo.java
+++ src/Jogo.java
@@ -1,6 +1,13 @@
 
 public class Jogo {
 
+    public int pontuacao1 = 0, pontuacao2 = 0;
+    public int rodadas = 20;
 
+    Jogo(int pont1, int pont2, int rod){
+        this.pontuacao1 = pont1;
+        this.pontuacao2 = pont2;
+        this.rodadas = rod;
+    }
 
 }
