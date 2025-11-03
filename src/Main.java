45f0b831ee50de2eb61d03b8ad4420a77a81a081
cassio-mendes <kassiogithub@gmail.com>
2025-11-02 21:32:20 -0300

    Começando a classe Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,63 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("-----BEM VINDO à CAÇA AO TESOURO!!!-----");
+        System.out.println("     Quais os jogadores de hoje?");
+        String nome1 = scanner.next();
+        String nome2 = scanner.next();
+
+        Jogador jogador1 = new Jogador(nome1);
+        Jogador jogador2 = new Jogador(nome2);
+
+        int opcao;
+        do {
+            mostrarMenuInicial(nome1, nome2);
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    jogar(jogador1, jogador2);
+                    break;
+
+                case 2:
+                    mostrarRegras();
+                    break;
+
+                case 3:
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Opção inválida... Tente de novo!");
+            }
+        } while(opcao != 1);
+    }
+
+    private static void mostrarMenuInicial(String nome1, String nome2) {
+        System.out.printf("\n-----%s e %s, escolham uma opção para prosseguir-----\n", nome1, nome2);
+        System.out.println("1 - Jogar");
+        System.out.println("2 - Regras");
+        System.out.println("3 - SAIR");
+        System.out.print("Digite aqui: ");
+    }
+
+    private static void mostrarRegras() {
+        System.out.println("""
+                \nNo início da partida, cada jogador irá informar as posições dos seus 8 tesouros no
+                tabuleiro 10x10. Deve-se informar a linha e a coluna do tesouro, que vão de 0 a 9. O jogo terá um total
+                de 20 rodadas, 10 para cada jogador. A cada rodada, vocês devem tentar adivinhar onde está o tesouro do
+                adversário, informando uma coordenada (linha e coluna) válida. Existem 3 tipos de tesouros:
+                Amarelos - 4 pontos; Verdes - 6 pontos; Vermelhos: 10 pontos. Cada jogador terá 3 Verdes, representados
+                pela letra 'G'; 3 Amarelos, de letra 'Y'; e 2 Vermelhos, de letra 'R'. O jogo termina quando todos os
+                tesouros de um jogador forem encontrados, ou depois de se passarem as 20 rodadas. Neste caso, vence
+                quem acumular mais pontos.""");
+    }
+
+    private static void jogar(Jogador jogador1, Jogador jogador2) {
 
     }
 
