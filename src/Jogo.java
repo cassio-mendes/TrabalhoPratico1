e233efa1e4d7720e61a87a406ed57bbe91e5660c
beatrizpaivafelix <140505743+beatrizpaivafelix@users.noreply.github.com>
2025-11-12 14:08:51 -0300

    tentando alternar jogador
    mexi só em "jogo"

--- src/Jogo.java
+++ src/Jogo.java
@@ -2,22 +2,22 @@
 public class Jogo {
 
     public int pontuacao1 = 0, pontuacao2 = 0;
-    public int rodadas = 20;
+    public int rodadas;
 
-    Jogo(int pont1, int pont2, int rod){
+    Jogo(int pont1, int pont2){
         this.pontuacao1 = pont1;
         this.pontuacao2 = pont2;
-        this.rodadas = rod;
+        this.rodadas = 20;
     }
 
     public boolean alternarPartidas(){
         while (rodadas > 0){
-            for(int jogador = 1; jogador <= 2; jogador++){
-                if(rodadas = 0){
-                    break;
+            for(int i = rodadas; i >= 0; i--){
+                if(i % 2 == 0){
+                    //jogador1 joga
+                } else {
+                    //jogador2 joga
                 }
-                Main main = new Main();
-                main.jogar(/* colocar um jogador só aqui, e ir alternando com o for */);
             }
         }
     }
